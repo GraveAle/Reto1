534b2cb01d6191f33dd3c0ed1cb17db52e7f1fb5
AleGh <alitosdas22@gmail.com>
2023-04-17 16:32:00 -0600

    Se agregan estilos

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
 
-var valor;
 
+//Funcion para encriptar texto
 function encriptarTexto (){
     var valor = document.getElementById('texto').value;
     valor = valor.toLowerCase().split('');
     
     for (i = 0; i < valor.length; i ++){
-    // var encriptado = [];
-    if (valor[i] == 'a') {
-    valor.splice([i],1, 'ai');
+
+        if (valor[i] == 'a') {
+            valor.splice([i],1, 'ai');
+        }
+        
+        if (valor[i] == 'e'){
+            valor.splice([i],1, 'enter');
+        }
+        
+        if (valor[i] == 'i'){
+            valor.splice([i],1, 'imes');
+        }
+        
+        if (valor[i] == 'o'){
+            valor.splice([i],1, 'ober');
+        }
+        
+        if (valor[i] == 'u'){
+            valor.splice([i],1, 'ufat');
+        }
+
     }
     
-    if (valor[i] == 'e'){
-    valor.splice([i],1, 'enter');
-    }
-    
-    if (valor[i] == 'i'){
-    valor.splice([i],1, 'imes');
-    }
-    
-    if (valor[i] == 'o'){
-    valor.splice([i],1, 'ober');
-    }
-    
-    if (valor[i] == 'u'){
-    valor.splice([i],1, 'ufat');
-    }
-    
-    var encriptado = valor;
-    document.getElementById('encriptado').innerHTML = encriptado;
-    }
+        valor = valor.join('');
+        mostrarTexto(valor);
+
+        limpiarTextoArea();
 }
 
+//Funcion para decodificar texto
 
-function mostrarTexto(){
-    document.getElementById('encriptado').innerHTML = valor;
+function desencriptarTexto (){
+    
+    var decodificar = document.getElementById('encriptado').value;
+    decodificar = decodificar.toLowerCase().split('');
+
+    for (i = 0; i < decodificar.length; i++){
+        if (decodificar[i] == 'a'){
+            decodificar.splice([i],1);
+            decodificar.splice([i],1, 'a')
+        }
+    }
+
+    console.log(decodificar);
+
+}
+
+// Funciones para limpiar pantalla
+function mostrarTexto(txt){
+    document.getElementById('encriptado').innerHTML = txt;
+}
+
+function limpiarTextoArea (){
+    document.getElementById('texto').value = "";
 }
 
